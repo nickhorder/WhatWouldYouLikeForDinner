da7ed876be090ef551c897b2380dfea2b76eb5c8
nickhorder <thunderstruck1986@hotmail.co.uk>
2021-05-08 19:27:31 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WhatWouldYouLikeForDinner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What would you like for dinner?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"Ah! {input} is an excellent choice!");
+        }
+    }
+}
